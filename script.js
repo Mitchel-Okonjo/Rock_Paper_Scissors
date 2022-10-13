a1f25a206683e937d211628ae758b890fe7a0da3
Mitchel-Okonjo <vokonjomitchel@gmai.com>
2022-10-13 14:17:55 +0100

    Add function to randomize computer's selection

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function getComputerChoice() {
+    let randInt = Math.floor(Math.random() * 3) + 1;
+    let choice;
+    if (randInt === 1) {
+        choice = "ROCK";
+    }
+    else if (randInt === 2) {
+        choice = "PAPER";
+    }
+    else {
+        choice = "SCISSORS";
+    }
+
+    return choice;
+}
+
+
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
@@ -5,6 +22,5 @@ function removeTransition(e) {
 const options = Array.from(document.querySelectorAll('.btn-yours'));
 options.forEach(option => option.addEventListener('click', () => option.classList.add('playing')));
 options.forEach(option => option.addEventListener('transitionend', removeTransition));
-
 
 
