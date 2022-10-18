c38c7bef23bd311ccf2fe704ca4ce24ec2d1ae2c
Mitchel-Okonjo <vokonjomitchel@gmai.com>
2022-10-18 02:44:10 +0100

    Add function to conduct one game round

--- script.js
+++ script.js
@@ -20,6 +20,49 @@ function removeTransition(e) {
     e.target.classList.remove('playing');
   }
 
+  function playRound(computerSelection, playerSelection) {
+    
+    if (computerSelection === "ROCK" && playerSelection === "SCISSORS") {
+        console.log("you lose");
+        round.textContent++;
+        roundEndText.textContent =  "You lose! Rock beats Scissors";
+    }
+    else if (computerSelection === "SCISSORS" && playerSelection === "PAPER") {
+        console.log("you lose");
+        round.textContent++;
+        roundEndText.textContent =  "You lose! Scissors beats Paper";
+    }
+    else if (computerSelection === "PAPER" && playerSelection === "ROCK") {
+        console.log("you lose");
+        round.textContent++;
+        roundEndText.textContent =  "You lose! Paper beats Rock";
+    }
+    else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+        console.log("you win");
+        round.textContent++;
+        score.textContent++;
+        roundEndText.textContent =  "You win! Rock beats Scissors";
+    }
+    else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        console.log("you win");
+        round.textContent++;
+        score.textContent++;
+        roundEndText.textContent =  "You win! Scissors beats Paper";
+    }
+    else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+        console.log("you win");
+        round.textContent++;
+        score.textContent++;
+        roundEndText.textContent = "You win! Paper beats Rock";
+    }
+    else {
+        console.log("It's a tie!");
+        roundEndText.textContent = "It's a tie! click an option to continue playing.";
+    }
+
+    
+}
+
 const computerOptions = Array.from(document.querySelectorAll('.btn-computer'));
 const playerOptions = Array.from(document.querySelectorAll('.btn-yours'));
 const computerSelection = document.querySelector('.btn-computer');
