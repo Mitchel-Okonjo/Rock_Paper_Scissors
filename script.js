5fe85fc43faf904a2bd8ba52ded7d43bed8212eb
Mitchel-Okonjo <vokonjomitchel@gmai.com>
2022-10-18 02:39:22 +0100

    Declare variables

--- script.js
+++ script.js
@@ -20,7 +20,16 @@ function removeTransition(e) {
     e.target.classList.remove('playing');
   }
 
+const computerOptions = Array.from(document.querySelectorAll('.btn-computer'));
 const playerOptions = Array.from(document.querySelectorAll('.btn-yours'));
+const computerSelection = document.querySelector('.btn-computer');
+const gameEndText = document.querySelector('.game-end-text');
+const gameStartText = document.querySelector('.game-start-text');
+const score = document.querySelector('.score-btn');
+const round = document.querySelector('.round-btn');
+const roundEndText = document.querySelector('.round-end-text');
+const restart = document.querySelector('.restart-btn');
+
 playerOptions.forEach(option => option.addEventListener('click', () => option.classList.add('playing')));
 playerOptions.forEach(option => option.addEventListener('transitionend', removeTransition));
 
